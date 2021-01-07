0466b6cd165dab49b2abdbc0585477986a8dda5e
Benjamin Alcala <benalcalajr@gmail.com>
2021-01-06 22:05:13 -0800

    Updates

---
+++ precache-manifest.e45fc1134846d5acec13c7ce71e9f092.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbebbb49a85d5cf8b5152df0c720e75",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "f9bbab9f4d454320c74f",
+    "url": "/pwa/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "80b197535e48198efa7b",
+    "url": "/pwa/static/css/main.050df1e5.chunk.css"
+  },
+  {
+    "revision": "f9bbab9f4d454320c74f",
+    "url": "/pwa/static/js/2.9e7a3f9a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pwa/static/js/2.9e7a3f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b197535e48198efa7b",
+    "url": "/pwa/static/js/main.69b972d8.chunk.js"
+  },
+  {
+    "revision": "dcadc55ee51e49fdd188",
+    "url": "/pwa/static/js/runtime-main.ea7309ff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
